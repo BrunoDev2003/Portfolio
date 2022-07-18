0009e47cd5c9a382fb9272fa9f51b6457a48755d
Bruno Gusmão <brunogusmao43@gmail.com>
2022-07-18 18:33:29 -0300

    colocando animações quando o scroll é usado

--- static/script.js
+++ static/script.js
@@ -21,3 +21,22 @@ navItem.forEach(item =>{
         menuMobile.classList.replace("bi-x", "bi-list");
     })
 })
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+    item.forEach((element) => {
+        if (windowTop> element.OffsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
